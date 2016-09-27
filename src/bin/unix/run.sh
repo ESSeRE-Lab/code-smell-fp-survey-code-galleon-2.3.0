1b39ee5087179f766b15f10ab1214d3517c1469c
inny <marco.zanoni@disco.unimib.it>
2016-09-27 15:38:09 +0200

    Initial import

---
+++ src/bin/unix/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Run the Galleon server
+#
+# You may have to set JAVA_HOME to the correct value for your system
+#JAVA_HOME=/usr/java/jre
+OLDCLASSPATH=$CLASSPATH
+CLASSPATH=../conf
+for j in ../lib/*.jar 
+do 
+CLASSPATH=$CLASSPATH:$j 
+done 
+
+OPTION=""
+if [ -f "$JAVA_HOME/bin/server" ]; then
+OPTION=-server
+fi
+java $OPTION -cp $CLASSPATH -Xms64m -Xmx64m -Djava.awt.fonts="$JAVA_HOME/lib/fonts" -Dawt.toolkit=com.eteks.awt.PJAToolkit org.lnicholls.galleon.server.Server
+CLASSPATH=$OLDCLASSPATH
